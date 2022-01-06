b40e242e2c2ee2b1b1d7a7dca7d7dca57cc99e5c
emily12jensen <90439014+emily12jensen@users.noreply.github.com>
2022-01-05 17:36:41 -0700

    Add project files.

---
+++ Myfirstaspwebapp/wwwroot/js/fanmail.js
@@ -0,0 +1,7 @@
+﻿document.getElementById("buttonsend").addEventListener("click", function (){
+    alert("From: " + document.getElementById("txtfrom").value +
+        ", Subject: " + document.getElementById("txtsubject").value +
+        ", message: " + document.getElementById("txtmessage").value);
+})
+
+$("#ganderson").fadeToggle();
